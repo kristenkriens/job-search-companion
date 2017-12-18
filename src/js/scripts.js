82f0667e1698a389ae7097e0574320f40157c3ac
Kristen Kriens <kristen.kriens@gmail.com>
2017-12-18 16:40:19 -0500

    Start adding js for call to indeed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,40 @@
 const app = {};
 
-app.getInfo = function() {
+app.indeedApiUrl = 'http://api.indeed.com/ads/apisearch';
+app.indeedApiKey = '';
+
+app.getIpAddress = function() {
+  $.ajax({
+		url: app.ipAddressFinderUrl,
+		method: 'GET',
+		dataType: 'json'
+	}).then(function(address) {
+		app.getJobs(address.ip);
+	});
+}
+
+app.getJobs = function(ipAddress) {
+  $.ajax({
+		url: indeedApiUrl,
+		method: 'GET',
+		dataType: 'jsonp',
+    data: {
+      publisher: app.indeedApiKey,
+      v: '2',
+      format: 'json',
+      useragent: navigator.userAgent,
+      userip: ipAddress,
+      limit: '200',
+      latLong: '1'
+    }
+	}).then(function(results) {
+		console.log(results);
+	});
 }
 
 // Initializes app
 app.init = function() {
-  app.getInfo();
+  app.getIpAddress();
 }
 
 $(function() {
